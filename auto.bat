0ce935dd0b95e9e26947f4a509aa0dda0ad41fc7
Olivier Hirsch <55246743+olivierhirsch@users.noreply.github.com>
2020-06-12 07:18:23 +0200

    Update auto.bat

--- auto.bat
+++ auto.bat
@@ -1,10 +1,10 @@
 cd c:\python38-32
 set $madate=%date:~-4%-%date:~3,2%-%date:~0,2%
-if exist solr_025.csv ren solr_025.csv solr_025-%$madate%.csv
+if exist solr025.csv ren solr25.csv solr025-%$madate%.csv
 python.exe solr_025.py
 python.exe resalistes-0.1-solr.py
 xcopy C:\Python38-32\%$madate% T:\deconfinement\drive\listes\%$madate% /E /C /R /H /I /K
-del solr_025.csv /f /q
+del solr025.csv /f /q
 
 
 ::Copyright (C) <2020>  >Olivier Hirsch> <Bibliothèque municipale de Reims>
